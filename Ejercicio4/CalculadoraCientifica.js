dac7f697ec26636308ae6649bff48ee6c821f6e8
luisfesu <luisfesu2000@gmail.com>
2021-11-11 00:13:17 +0100

    calculadora cientifica a medias

---
+++ Ejercicio4/CalculadoraCientifica.js
@@ -0,0 +1,246 @@
+"use strict";
+class Calculadora {
+  constructor(nombre) {
+    this.nombre = nombre;
+    this.currentScreenNumber = "";
+    this.operator = "";
+    this.val1 = "";
+    this.memory = 0;
+
+    document.addEventListener("keydown", (event) => {
+      const keyName = event.key;
+
+      switch (keyName) {
+        case "7":
+          calculadora.add(7);
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "8":
+          calculadora.add(8);
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "9":
+          calculadora.add(9);
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "4":
+          calculadora.add(4);
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "5":
+          calculadora.add(5);
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "6":
+          calculadora.add(6);
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "1":
+          calculadora.add(1);
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "2":
+          calculadora.add(2);
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "3":
+          calculadora.add(3);
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "/":
+          calculadora.operacion("/");
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "*":
+          calculadora.operacion("*");
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "-":
+          calculadora.operacion("-");
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "+":
+          calculadora.operacion("+");
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case ".":
+          calculadora.addDecimal();
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "c":
+          calculadora.clear();
+          calculadora.mostrarEnPantalla("screen");
+          break;
+        case "Enter":
+          calculadora.igual();
+          break;
+      }
+    });
+  }
+
+  clear() {
+    this.currentScreenNumber = "";
+    this.operator = "";
+    this.val1 = "";
+    this.memory = 0;
+  }
+
+  addToMem() {
+    if (this.currentScreenNumber.length > 0) {
+      this.memory += Number(this.currentScreenNumber);
+    }
+  }
+
+  substractFromMem() {
+    if (this.currentScreenNumber.length > 0) {
+      this.memory -= Number(this.currentScreenNumber);
+    }
+  }
+
+  memToScreen() {
+    this.currentScreenNumber = this.memory.toString();
+  }
+
+  mostrarEnPantalla(idPantalla) {
+    document.getElementById(idPantalla).value = this.currentScreenNumber;
+  }
+
+  add(valor) {
+    if (this.currentScreenNumber.length < 9) {
+      this.currentScreenNumber += valor.toString();
+    }
+  }
+
+  addDecimal() {
+    if (!this.currentScreenNumber.includes(".")) {
+      this.currentScreenNumber += ".";
+    } else if (this.currentScreenNumber.length <= 0) {
+      this.currentScreenNumber += "0.";
+    }
+  }
+
+  operacion(operator) {
+    if (
+      this.val1.toString().length <= 0 &&
+      this.currentScreenNumber.length <= 0
+    ) {
+      // Cuando pulsamos un operador sin haber introducido ninugn numero
+      this.val1 = "0";
+      this.currentScreenNumber = "";
+      this.operator = operator;
+    } else if (
+      this.val1.toString().length <= 0 ||
+      (this.val1.toString().length > 0 && this.currentScreenNumber.length > 0)
+    ) {
+      // Cuando aun no hemos almacenado nada en val1, ni hemos pulsado previamente el igual
+      this.val1 = this.currentScreenNumber;
+      this.currentScreenNumber = "";
+      this.operator = operator;
+    } else {
+      // cuando ya tenemos un valor en val1 (resultado) y añadimos un operador sin que haya un numero en pantalla
+      this.currentScreenNumber = "";
+      this.operator = operator;
+    }
+  }
+
+  igual() {
+    if (this.operator.length > 0) {
+      try {
+        this.val1 = eval(
+          Number(this.val1) + this.operator + Number(this.currentScreenNumber)
+        );
+      } catch (err) {
+        this.val1 = "Error: " + err;
+      }
+
+      this.currentScreenNumber = "";
+      this.operator = "";
+    } else if (this.currentScreenNumber.length > 0) {
+      try {
+        this.val1 = eval(this.currentScreenNumber);
+      } catch (Err) {
+        this.val1 = "Error: " + Err;
+      }
+    }
+
+    document.getElementById("screen").value = this.val1;
+  }
+}
+
+class CalculadoraCientifica extends Calculadora {
+  constructor(nombre) {
+    super(nombre);
+  }
+
+  memoryClear() {
+    this.memory = 0;
+  }
+
+  clearCurrentNumber() {
+    this.currentScreenNumber = "";
+  }
+
+  mostrarEnPantalla(idPantalla) {
+    document.getElementById(idPantalla).value =
+      this.val1 + "\n" + this.currentScreenNumber;
+  }
+
+  square() {
+    if (this.currentScreenNumber.length > 0) {
+      this.currentScreenNumber = Math.pow(
+        Number(this.currentScreenNumber),
+        2
+      ).toString();
+    } else if (this.val1.toString().length > 0) {
+      this.currentScreenNumber = Math.pow(Number(this.val1), 2).toString();
+    }
+  }
+
+  squareRoot() {
+    if (this.currentScreenNumber.length > 0) {
+      this.currentScreenNumber = Math.sqrt(
+        Number(this.currentScreenNumber)
+      ).toString();
+    } else if (this.val1.toString().length > 0) {
+      this.currentScreenNumber = Math.sqrt(Number(this.val1)).toString();
+    }
+  }
+
+  seno() {
+    if (this.currentScreenNumber.length > 0) {
+      this.currentScreenNumber = Math.sin(
+        Number(this.currentScreenNumber)
+      ).toString();
+    } else if (this.val1.toString().length > 0) {
+      this.currentScreenNumber = Math.sin(Number(this.val1)).toString();
+    }
+  }
+
+  coseno() {
+    if (this.currentScreenNumber.length > 0) {
+      this.currentScreenNumber = Math.cos(
+        Number(this.currentScreenNumber)
+      ).toString();
+    } else if (this.val1.toString().length > 0) {
+      this.currentScreenNumber = Math.cos(Number(this.val1)).toString();
+    }
+  }
+
+  tangente() {
+    if (this.currentScreenNumber.length > 0) {
+      this.currentScreenNumber = Math.tan(
+        Number(this.currentScreenNumber)
+      ).toString();
+    } else if (this.val1.toString().length > 0) {
+      this.currentScreenNumber = Math.tan(Number(this.val1)).toString();
+    }
+  }
+
+  delete() {
+    if (this.currentScreenNumber.length > 0) {
+      this.currentScreenNumber = this.currentScreenNumber.slice(0, -1);
+    }
+  }
+}
+
+var calculadora = new CalculadoraCientifica("Calculadora Básica");
